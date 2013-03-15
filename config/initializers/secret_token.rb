18ce077180a2962370022ac4c11bb97ce71a1c27
Benjamin Larson <ben@slimdesign.co>
2013-03-15 15:03:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hive::Application.config.secret_token = '410c02959c34540065f60e35d4f902386af7f0531f17fa60cd49b6b48d13bcb861403f6c89f9e8a5b1afa4745937ed19e363f0b2b4578132fed764e1eb098f55'
